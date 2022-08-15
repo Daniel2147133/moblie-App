7ebfad12b8dc480fe2b0f5877443904defdec253
Daniel2147133 <95094232+Daniel2147133@users.noreply.github.com>
2022-08-15 20:39:48 +0530

    a

--- BikeRENTAL/BikeRENTAL/app/src/main/java/com/example/bikerental/splashscreen.java
+++ BikeRENTAL/BikeRENTAL/app/src/main/java/com/example/bikerental/splashscreen.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
+
 public class splashscreen extends AppCompatActivity   {
     private Thread run;
+    LottieAnimationView lottieAnimationView,lottieAnimationView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        lottieAnimationView=findViewById(R.id.animation_view);
+        lottieAnimationView2=findViewById(R.id.animation_view2);
+
+        lottieAnimationView.playAnimation();
+        lottieAnimationView2.playAnimation();
+
         Thread thread = new Thread(){
             public void run(){
                 try {
